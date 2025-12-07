2ae428b3ca91a3108b3e6e55bdf31d1a52deefca
Sam <sukhrobbekrustamov26@gmail.com>
2025-12-07 15:16:22 -0800

    robot

--- Steps.py
+++ Steps.py
@@ -1,8 +1,15 @@
 class Person:
 
-    def __init__(self, name, steps_today):
+    def __init__(self, name, steps_today, step):
         self.name = name
         self.steps_today = steps_today
+        self.step = step
 
     def add_steps(self, count):
-        
+        self.steps_today += count
+
+    def goal_reached(self, goal):
+        if self.steps_today > goal:
+            "True"
+        else:
+            "False"
